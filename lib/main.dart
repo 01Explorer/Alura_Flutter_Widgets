c8c36bc4caa416930bf52be014c8011a7760b930
Gabriel <gabriel.s.pb@compasso.com.br>
2022-09-21 11:51:16 -0300

    feat Adicionada capacidade de ocultar as tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
 
   @override
   Widget build(BuildContext context) {
@@ -19,38 +26,51 @@ class MyApp extends StatelessWidget {
             leading: Container(),
             title: const Text('Tarefas'),
           ),
-          body: ListView(
-            children: [
-              Task(
-                nome: 'Andar de Bike',
-                foto:
-                    'https://www.osnibike.com.br/arquivos/LoginID_436/Padrao/infobicicleta2-4408.jpg',
-                dificuldade: 2,
-              ),
-              Task(
-                nome: 'Ler Clean code',
-                foto:
-                    'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
-                dificuldade: 4,
-              ),
-              Task(
-                nome: 'Estudar',
-                foto:
-                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                dificuldade: 4,
-              ),
-              Task(
-                nome: 'Meditar',
-                foto:
-                    'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
-                dificuldade: 5,
-              ),
-              Task(
-                nome: 'Jogar a vida fora',
-                foto: 'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg',
-                dificuldade: 1,
-              ),
-            ],
+          body: AnimatedOpacity(
+            opacity: opacidade ? 1 : 0,
+            duration: Duration(milliseconds: 800),
+            child: ListView(
+              children: [
+                Task(
+                  nome: 'Andar de Bike',
+                  foto:
+                      'https://www.osnibike.com.br/arquivos/LoginID_436/Padrao/infobicicleta2-4408.jpg',
+                  dificuldade: 2,
+                ),
+                Task(
+                  nome: 'Ler Clean code',
+                  foto:
+                      'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
+                  dificuldade: 4,
+                ),
+                Task(
+                  nome: 'Estudar',
+                  foto:
+                      'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                  dificuldade: 4,
+                ),
+                Task(
+                  nome: 'Meditar',
+                  foto:
+                      'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
+                  dificuldade: 5,
+                ),
+                Task(
+                  nome: 'Jogar a vida fora',
+                  foto:
+                      'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg',
+                  dificuldade: 1,
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                opacidade = !opacidade;
+              });
+            },
+            child: const Icon(Icons.remove_red_eye),
           ),
         ));
   }
