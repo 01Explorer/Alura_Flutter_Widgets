32cb5d4446ae2052bc5c18c0cf604e3fb3a73adf
Gabriel <gabriel.s.pb@compasso.com.br>
2022-09-20 22:49:01 -0300

    Introduzida List View e Text Overflow

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,26 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text('Tarefas'),
           ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          body: ListView(
             children: [
-              Task(nome: 'Andar de Bike',),
-              Task(nome: 'Ler Clean code',),
-              Task(nome: 'Estudar',),
+              Task(
+                nome: 'Andar de Bike com a minha namorada pq eu não tenho ideia doq colocar',
+              ),
+              Task(
+                nome: 'Ler Clean code',
+              ),
+              Task(
+                nome: 'Estudar',
+              ),
+              Task(
+                nome: 'Não sei',
+              ),
+              Task(
+                nome: 'Halo',
+              ),
+              Task(
+                nome: 'Hey',
+              ),
             ],
           ),
           floatingActionButton: FloatingActionButton(onPressed: () {
@@ -43,35 +57,50 @@ class Task extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Container(
-            color: Colors.blue,
-            height: 140,
-          ),
-          Container(
-            color: Colors.white,
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  color: Colors.black26,
-                  width: 72,
-                  height: 100,
-                ),
-                Text(nome),
-                ElevatedButton(
-                  child: Column(
-                    children: const [Icon(Icons.arrow_drop_up), Text('Lvl Up')],
-                  ),
-                  onPressed: () {},
-                )
-              ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
             ),
-          )
-        ],
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.black26,
+                    width: 72,
+                    height: 100,
+                  ),
+                  Container(
+                    width: 200,
+                    child: Text(
+                      nome,
+                      style: TextStyle(
+                        fontSize: 24,
+                        overflow: TextOverflow.ellipsis
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    child: Column(
+                      children: const [
+                        Icon(Icons.arrow_drop_up),
+                        Text('Lvl Up')
+                      ],
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
